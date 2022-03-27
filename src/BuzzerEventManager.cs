8a63fe4c9e60bf4759f5390542e112e22ef44c29
matheusraffonso <100166867+matheusraffonso@users.noreply.github.com>
2022-03-26 19:51:48 -0700

    Refactoring functions files (#5)
    * refactoring functions files
    * removing unnecessary package references

---
+++ src/BuzzerEventManager.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace BuzzerBot
+{
+  public class BuzzerEventManager
+  {
+    public static string BUZZER_EVENT = "BuzzerEvent";
+
+    public static async Task RaiseBuzzerEvent(
+        IDurableOrchestrationClient orchestrationClient,
+        BuzzerEvent buzzerEvent,
+        ILogger logger,
+        BuzzerEventPayload payload = null)
+    {
+      OrchestrationStatusQueryResult result = await orchestrationClient.ListInstancesAsync(
+          new OrchestrationStatusQueryCondition { RuntimeStatus = new[] { OrchestrationRuntimeStatus.Running } },
+          CancellationToken.None
+      );
+      IEnumerable<DurableOrchestrationStatus> instances = result.DurableOrchestrationState;
+
+      if (!instances.Any())
+      {
+        logger.LogInformation("No ongoing calls found to raise an event");
+        return;
+      }
+
+      payload ??= new BuzzerEventPayload();
+      string instanceId = instances.Single().InstanceId;
+
+      await orchestrationClient.RaiseEventAsync(instanceId, BuzzerEventManager.BUZZER_EVENT, (buzzerEvent, payload));
+    }
+  }
+}
